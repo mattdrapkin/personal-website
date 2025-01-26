0177e498b0b8f712c39342ae4d90e32b7c36553a
Matt Drapkin <matt@drapkin.org>
2025-01-25 22:05:59 -0800

    More fixing build errors, template assumes .env vars are set

--- src/env/server.ts
+++ src/env/server.ts
@@ -4,27 +4,27 @@ import { z } from 'zod';
 export const serverEnvs = createEnv({
     server: {
         NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
-        DATABASE_URL: z.string(),
+        DATABASE_URL: z.string().default('postgresql://postgres:232409@localhost:5432/personal-website'),
 
         EMAIL_PROVIDER: z.enum(['console', 'smtp', 'resend']).default('console'),
 
         // if email provider is smtp
-        SMTP_HOST: z.string().optional(),
+        SMTP_HOST: z.string().default('default'),
         SMTP_PORT: z.preprocess(Number, z.number()).default(587),
-        SMTP_USERNAME: z.string().optional(),
-        SMTP_PASSWORD: z.string().optional(),
+        SMTP_USERNAME: z.string().default('default'),
+        SMTP_PASSWORD: z.string().default('default'),
         SMTP_SECURE: z.preprocess(v => v === 'true' || v === '1', z.boolean()).default(true),
 
         // if email provider is resend
-        RESEND_API_KEY: z.string().optional(),
+        RESEND_API_KEY: z.string().default('default'),
 
         // always required regardless of email provider
-        EMAIL_FROM: z.string(),
+        EMAIL_FROM: z.string().default('default'),
 
         STANDALONE: z.coerce.number().default(0),
     },
     experimental__runtimeEnv: process.env,
-    emptyStringAsUndefined: true,
+    emptyStringAsUndefined: false,
 });
 
 export type ServerEnvs = typeof serverEnvs;
