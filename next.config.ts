ec5e5b33c4ae56f7261e8b9554d5f4da66c46522
Matt Drapkin <matt@drapkin.org>
2025-01-25 16:43:57 -0800

    Initial commit from Create Next App

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import '@/env/client';
+import { serverEnvs } from '@/env/server';
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+    output: serverEnvs.STANDALONE === 1 ? 'standalone' : undefined,
+    reactStrictMode: true,
+    serverExternalPackages: ['@node-rs/argon2'],
+};
+
+export default nextConfig;
